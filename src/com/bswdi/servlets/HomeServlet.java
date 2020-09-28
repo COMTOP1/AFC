d3909ab17845736103a8ac21e6b2cfe1df00bec1
Liam Burnand <liamb1216@gmail.com>
2020-09-28 21:11:11 +0100

    General code cleanup

--- src/com/bswdi/servlets/HomeServlet.java
+++ src/com/bswdi/servlets/HomeServlet.java
@@ -2,12 +2,9 @@ package com.bswdi.servlets;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
 /**
  * Home servlet
